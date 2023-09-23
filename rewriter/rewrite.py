57a6a185f7c780d186d784abc386f7c0d0022bf3
Ole van Santen <ovs@netcompany.com>
2023-09-23 19:41:23 +0200

    Making the Module report on the original c code

--- rewriter/rewrite.py
+++ rewriter/rewrite.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+import json
 import sys
 import os
 
@@ -15,13 +16,12 @@ def get_library_file_name(file_path):
 
     return os.path.join(file_folder, "library.js")
 
-def get_temp_file_name(file_path): 
+def get_temp_file_name(file_path, file_extension): 
     file_path_components = os.path.split(file_path)
     file_name_and_extension = file_path_components[-1].rsplit('.', 1)
     print(file_path_components)
     file_folder = '/'.join(file_path_components[:-1])
     file_name = file_name_and_extension[0]
-    file_extension = file_name_and_extension[1]
 
     if file_folder:
         return os.path.join(file_folder, file_name + ".g." + file_extension)
@@ -36,6 +36,18 @@ def get_output_file_name(file_path):
         return os.path.join(file_folder, "output.js")
     else: 
         return "output.js"
+
+def start_meta_write(file_path1, file_path2): 
+    c_file = open(file_path1, "r")
+    c_code = c_file.read()
+    c_code_escaped = json.dumps(c_code)
+
+    js_code = ("var Module = Module || { };\n"
+               "Module.preRun = Module.preRun || [];\n"
+               f"Module.preRun.push(function() {{ Module.simulatorCode = {c_code_escaped} }})")
+    js_file = open(file_path2, "w")
+    js_file.write(js_code)
+    js_file.close()
 
 def start_rewrite(file_path1, file_path2):
     ast = parse_file(file_path1, use_cpp=True, cpp_path= 'clang', cpp_args= ['-E'])
@@ -57,8 +69,8 @@ def start_rewrite(file_path1, file_path2):
     output_f.write(transformed_code)
     output_f.close()
 
-def start_transpile(file_path1, file_path2, file_path3): 
-    os.system('emcc %s -s WASM=1 -o %s -s "EXPORTED_FUNCTIONS=[\'_main\']" --js-library %s' % (file_path1, file_path2, file_path3))
+def start_transpile(file_path1, file_path2, file_path3, file_path4): 
+    os.system('emcc %s -s WASM=1 -o %s -s "EXPORTED_FUNCTIONS=[\'_main\']" --pre-js %s --js-library %s' % (file_path1, file_path2, file_path3, file_path4))
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -68,8 +80,10 @@ if __name__ == "__main__":
     library_file = get_library_file_name(script_file)
 
     input_file = sys.argv[1]
-    temp_file = get_temp_file_name(input_file)
-    output_file = get_output_file_name(temp_file)
+    temp_c_file = get_temp_file_name(input_file, 'c')
+    temp_js_file = get_temp_file_name(input_file, 'js')
+    output_file = get_output_file_name(temp_c_file)
 
-    start_rewrite(input_file, temp_file)
-    start_transpile(temp_file, output_file, library_file)
+    start_meta_write(input_file, temp_js_file)
+    start_rewrite(input_file, temp_c_file)
+    start_transpile(temp_c_file, output_file, temp_js_file, library_file)
