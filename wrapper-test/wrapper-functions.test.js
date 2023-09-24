999ac63d5dc9129f9f6fe7f690907eb9c3d22db0
Ole van Santen <ovs@netcompany.com>
2023-09-24 15:12:25 +0200

    Implementing getEvaluatedCode()

--- wrapper-test/wrapper-functions.test.js
+++ wrapper-test/wrapper-functions.test.js
@@ -1,10 +1,85 @@
-var assert = require('assert');
-var functions = require('../wrapper/wrapper-functions.js');
+import assert from 'assert';
+import { stepForward, stepBackwards, getEvaluatedCode } from '../wrapper/wrapper-functions.js';
 
-describe('Array', function () {
-    describe('#indexOf()', function () {
-      it('should return -1 when the value is not present', function () {
-        assert.equal([1, 2, 3].indexOf(4), -1);
-      });
-    });
+describe('getEvaluatedCode', function () {
+  it('replaces expression at beginning', function () {
+    const code = "5 * 7 + 6;";
+    const steps = [{
+      type: 'expression',
+      dataType: "int",
+      dataValue: 35,
+      location: [1, 1, 1, 5]
+    }];
+
+    const actual = getEvaluatedCode(code, steps);
+    const expected = "35 + 6;";
+    assert.equal(actual, expected);
   });
+  it('replaces expression in middle', function () {
+    const code = "f(5 * 7);";
+    const steps = [{
+      type: 'expression',
+      dataType: "int",
+      dataValue: 35,
+      location: [1, 3, 1, 7]
+    }];
+
+    const actual = getEvaluatedCode(code, steps);
+    const expected = "f(35);";
+    assert.equal(actual, expected);
+  });
+  it('replaces expression at end', function () {
+    const code = "6 + 5 * 7;";
+    const steps = [{
+      type: 'expression',
+      dataType: "int",
+      dataValue: 35,
+      location: [1, 5, 1, 9]
+    }];
+
+    const actual = getEvaluatedCode(code, steps);
+    const expected = "6 + 35;";
+    assert.equal(actual, expected);
+  });
+  it('replaces non-overlapping expressions', function () {
+    const code = "5 * 7 + 6 * 3;";
+    const steps = [
+      {
+        type: 'expression',
+        dataType: "int",
+        dataValue: 35,
+        location: [1, 1, 1, 5]
+      }, {
+        type: 'expression',
+        dataType: 'int',
+        dataValue: 18,
+        location: [1, 9, 1, 13]
+      }
+    ];
+
+    const actual = getEvaluatedCode(code, steps);
+    const expected = "35 + 18;";
+    assert.equal(actual, expected);
+  });
+  it('replaces overlapped expressions', function () {
+    const code = "5 * 7 + 6;";
+    const steps = [
+      {
+        type: 'expression',
+        dataType: "int",
+        dataValue: 35,
+        location: [1, 1, 1, 5]
+      },
+      {
+        type: 'expression',
+        dataType: "int",
+        dataValue: 41,
+        location: [1, 1, 1, 9]
+      }
+    ];
+
+    const actual = getEvaluatedCode(code, steps);
+    const expected = "41;";
+    assert.equal(actual, expected);
+  });
+});
--- wrapper/wrapper-functions.js
+++ wrapper/wrapper-functions.js
@@ -12,7 +12,7 @@
  * @param {"expression"} mode
  * @returns {number}
  */
-function stepForward(steps, currentStep, mode) {
+export function stepForward(steps, currentStep, mode) {
     if (mode !== "expression")
         throw new error("Unsupported mode " + mode);
 
@@ -26,7 +26,7 @@ function stepForward(steps, currentStep, mode) {
  * @param {"expression"} mode
  * @returns {number}
  */
-function stepBackwards(steps, currentStep, mode) {
+export function stepBackwards(steps, currentStep, mode) {
     if (mode !== "expression")
         throw new error("Unsupported mode " + mode);
 
@@ -38,16 +38,78 @@ function stepBackwards(steps, currentStep, mode) {
  * @param {string} code 
  * @param {SimulationStep[]} steps 
  */
-function getEvaluatedCode(code, steps) {
+export function getEvaluatedCode(code, steps) {
+    // TODO implement clear for active-statement-change 
 
+    // Filters out steps with encompased by other steps
+    const expressionSteps = steps.filter(s => s.type === "expression");
+    const activeExpressionSteps = expressionSteps.reduce(
+        (steps, value) => [...steps.filter(s => !isLocationWithinLocation(s.location, value.location)), value],
+        []
+    );
+    // Ordering steps according to start line and start charachter 
+    activeExpressionSteps.sort(
+        (s1, s2) => (s1.location[0] == s2.location[0]) ?  s1.location[1] - s2.location[1] : s1.location[0] - s2.location[0]
+    );
+
+    return getEvaluatedCodeInternal(code, activeExpressionSteps);
 }
 
-/**
- * @param {SimulationStep[]} steps
- */
-function applyCodeChange(steps) {
-    
+// Calculates evaluated code from a list of ordered non-overlapping steps
+function getEvaluatedCodeInternal(code, activeSteps) {
+    let buffer = "";
+
+    for(let i = 0; i < activeSteps.length; i++) {
+        const step = activeSteps[i];
+        const nextStep = (i < activeSteps.length - 1) ? activeSteps[i + 1] : undefined;
+
+        if (i == 0) buffer += getContentBeforeLocation(code, step.location);
+        buffer += step.dataValue + "";
+        if (nextStep) buffer += getContentBetweenLocations(code, step.location, nextStep.location);
+        else buffer += getContentAfterLocation(code, step.location);
+    }
+    return buffer; 
 }
 
+function isLocationWithinLocation(subLocation, location) {
+    // (sub)location = [start line, start char, end line, start char]
+    const isStartWithinLocation = (subLocation[0] > location[0]) || (subLocation[0] == location[0] && subLocation[1] >= location[1]);
+    const isEndwithinLocation = (subLocation[2] < location[2]) || (subLocation[2] == location[2] && subLocation[3] <= location[3]);
+
+    return isStartWithinLocation && isEndwithinLocation;
+}
+
+function getContentBeforeLocation(code,location) {
+    return getContentWithinLocation(code, [1, 1, location[0], location[1]]);
+}
+
+function getContentBetweenLocations(code, location1, location2) {
+    return getContentWithinLocation(code, [location1[2], location1[3] + 1, location2[0], location2[1]]);
+}
+
+function getContentWithinLocation(code, location) {
+    const lines = code.split('\n');
+
+    if (location[0] != location[2]) {
+        return [
+            lines[location[0] - 1].substring(location[1]),
+            ...lines.filter((_, i) => location[0] + 1 < i && i < location[1] - 1),
+            lines[location[2] - 1].substring(0, location[3])
+        ].join("\n");
+    }
+    return lines[location[0] - 1].substring(location[1] - 1, location[3] - 1);
+}
+
+function getContentAfterLocation(code, location) {
+    const lines = code.split('\n');
+
+    if (location[2] < lines.length - 1) {
+        return [
+            lines[location[2] - 1].substring(location[3]),
+            ...lines.filter((_, i) => i > location[2])
+        ].join("\n");
+    }
+    return lines[location[2] - 1].substring(location[3])
+}
 
 export default { stepForward, stepBackwards, getEvaluatedCode }
