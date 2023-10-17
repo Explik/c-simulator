4f6ecf4a87afb30fac1b4d4a21bfa392dd5cb292
Ole van Santen <ovs@netcompany.com>
2023-10-17 21:05:38 +0200

    Temporary adaption of library.js to handle other actions than eval in simulator steps

--- rewriter/library.js
+++ rewriter/library.js
@@ -1,19 +1,22 @@
 mergeInto(LibraryManager.library, {
     notify: function(metadata, dataPtr) {
-        // For the purpose of this example, I'm assuming `data` is an integer
         var metadataValue = UTF8ToString(metadata);
-        var dataValue = Module.HEAP32[dataPtr / 4];
-        
-        var parsedMetadata = metadataValue.split(';').map(s => s.split('='))
-        var parsedType = parsedMetadata.find(s => s[0] === 't')[1];
-        var parsedLocation = JSON.parse(parsedMetadata.find(s => s[0] === 'l')[1]);
-        
-        Module.simulatorSteps = Module.simulatorSteps || [];
-        Module.simulatorSteps.push({
-            type: "expression",
-            location: parsedLocation,
-            dataType: parsedType,
-            dataValue: dataValue,
-        });
+
+        if (metadataValue.includes("a=eval")) {
+            // For the purpose of this example, I'm assuming `data` is an integer
+            var dataValue = Module.HEAP32[dataPtr / 4];
+
+            var parsedMetadata = metadataValue.split(';').map(s => s.split('='))
+            var parsedType = parsedMetadata.find(s => s[0] === 't')[1];
+            var parsedLocation = JSON.parse(parsedMetadata.find(s => s[0] === 'l')[1]);
+
+            Module.simulatorSteps = Module.simulatorSteps || [];
+            Module.simulatorSteps.push({
+                type: "expression",
+                location: parsedLocation,
+                dataType: parsedType,
+                dataValue: dataValue,
+            });
+        }
     }
 });
