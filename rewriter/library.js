a5b29459c2f2f656849aa53229a33dda5090ca74
Ole van Santen <ovs@netcompany.com>
2023-10-19 20:08:54 +0200

    Reworking step generation

--- rewriter/library.js
+++ rewriter/library.js
@@ -1,22 +1,55 @@
 mergeInto(LibraryManager.library, {
     notify: function(metadata, dataPtr) {
-        var metadataValue = UTF8ToString(metadata);
-
-        if (metadataValue.includes("a=eval")) {
-            // For the purpose of this example, I'm assuming `data` is an integer
-            var dataValue = Module.HEAP32[dataPtr / 4];
-
-            var parsedMetadata = metadataValue.split(';').map(s => s.split('='))
-            var parsedType = parsedMetadata.find(s => s[0] === 't')[1];
-            var parsedLocation = JSON.parse(parsedMetadata.find(s => s[0] === 'l')[1]);
-
-            Module.simulatorSteps = Module.simulatorSteps || [];
-            Module.simulatorSteps.push({
-                type: "expression",
-                location: parsedLocation,
-                dataType: parsedType,
-                dataValue: dataValue,
-            });
+        // Parse raw data
+        var getValue = (arr, name) => arr.find(i => i[0] == name) && arr.find(i => i[0] == name)[1];
+        var properties = UTF8ToString(metadata).split(';').map(s => s.split('='));
+        
+        var dataValue;
+        var dataType = getValue(properties, "t");
+        switch(dataType) {
+            case "int": 
+                dataValue = Module.HEAP32[dataPtr / 4];
+                break;
+            default: 
+                throw new Error("Unsupported data type " + dataType);
         }
+
+        var rawLocation = getValue(properties, "l");
+        var location = rawLocation && rawLocation.substring(1, rawLocation.length - 1).split(",").map(n => parseInt(n));
+        
+        // Create step 
+        var simulatorStep;
+        switch(getValue(properties, "a")) {
+            case "assign": 
+                simulatorStep = {
+                    action: "assign",
+                    identifier: getValue(properties, "i"),
+                    dataType: dataType,
+                    dataValue: dataValue
+                };
+                break;
+            case "decl": 
+                simulatorStep = {
+                    action: "decl",
+                    identifier: getValue(properties, "i"),
+                    dataType: dataType,
+                    dataValue: dataValue
+                };
+                break;
+            case "eval": 
+                simulatorStep = {
+                    action: "eval",
+                    location: location,
+                    dataType: dataType,
+                    dataValue: dataValue
+                }
+                break;
+            default:
+                throw new Error("Unsupported " + getValue(properties, "a"))
+            
+        }
+
+        Module.simulatorSteps = Module.simulatorSteps || [];
+        Module.simulatorSteps.push(simulatorStep);
     }
 });
