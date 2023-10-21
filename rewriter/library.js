9e2f5edcfb511175324ca0d0403130af2ba13141
Ole van Santen <ovs@netcompany.com>
2023-10-21 16:43:37 +0200

    Reworking step creation from notify()

--- rewriter/library.js
+++ rewriter/library.js
@@ -1,55 +1,42 @@
 mergeInto(LibraryManager.library, {
-    notify: function(metadata, dataPtr) {
-        // Parse raw data
-        var getValue = (arr, name) => arr.find(i => i[0] == name) && arr.find(i => i[0] == name)[1];
-        var properties = UTF8ToString(metadata).split(';').map(s => s.split('='));
-        
-        var dataValue;
-        var dataType = getValue(properties, "t");
-        switch(dataType) {
-            case "int": 
-                dataValue = Module.HEAP32[dataPtr / 4];
-                break;
-            default: 
-                throw new Error("Unsupported data type " + dataType);
-        }
+    notify: function(metadataPtr, dataPtr) {
+        var step = {}; 
 
-        var rawLocation = getValue(properties, "l");
-        var location = rawLocation && rawLocation.substring(1, rawLocation.length - 1).split(",").map(n => parseInt(n));
+        // Parse raw data
+        var metadata = UTF8ToString(metadataPtr);
+        var parsedMetadata = metadata
+            .split(';')
+            .map(s => s.split('='))
+            .reduce((acc, v) => ({ ...acc, [v[0]]: v[1] }), {});
+        
+        if ("a" in parsedMetadata) {
+            step.action = parsedMetadata["a"];
+        } 
+        else throw new Error("Missing property a");
+
+        if ("i" in parsedMetadata) {
+            step.identifier = parsedMetadata["i"];
+        }
+        if ("l" in parsedMetadata) {
+            step.location = parsedMetadata["l"]
+                .substring(1, parsedMetadata["l"].length - 1)
+                .split(",")
+                .map(n => parseInt(n));
+        }
+        if ("t" in parsedMetadata) {
+            step.dataType = parsedMetadata["t"];
+
+            switch(parsedMetadata["t"]) {
+                case "int":
+                    step.dataValue = Module.HEAP32[dataPtr / 4];
+                    break;
+                default: 
+                    throw new Error("Unsupported data type " + parsedMetadata["t"]);
+            }
+        }
         
         // Create step 
-        var simulatorStep;
-        switch(getValue(properties, "a")) {
-            case "assign": 
-                simulatorStep = {
-                    action: "assign",
-                    identifier: getValue(properties, "i"),
-                    dataType: dataType,
-                    dataValue: dataValue
-                };
-                break;
-            case "decl": 
-                simulatorStep = {
-                    action: "decl",
-                    identifier: getValue(properties, "i"),
-                    dataType: dataType,
-                    dataValue: dataValue
-                };
-                break;
-            case "eval": 
-                simulatorStep = {
-                    action: "eval",
-                    location: location,
-                    dataType: dataType,
-                    dataValue: dataValue
-                }
-                break;
-            default:
-                throw new Error("Unsupported " + getValue(properties, "a"))
-            
-        }
-
         Module.simulatorSteps = Module.simulatorSteps || [];
-        Module.simulatorSteps.push(simulatorStep);
+        Module.simulatorSteps.push(step);
     }
 });
