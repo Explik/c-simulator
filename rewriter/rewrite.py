bba93291b8724d34d371c4e6f10788dc6375da01
Ole van Santen <ovs@netcompany.com>
2023-10-22 15:47:22 +0200

    Fixing styling

--- rewriter/rewrite.py
+++ rewriter/rewrite.py
@@ -50,8 +50,8 @@ def start_meta_write(file_path1, file_path2):
     c_code_escaped = json.dumps(c_code)
 
     js_code = ("var Module = Module || { };\n"
-               "Module.print = function() { \n   Module.simulatorSteps = Module.simulatorSteps || [];\n   Module.simulatorSteps.push({ action: \"stdout\", value: Array.from(arguments).join(\"\") + \"\\n\"});\n}\n"
-               "Module.printErr = function() { \n   Module.simulatorSteps = Module.simulatorSteps || [];\n   Module.simulatorSteps.push({ action: \"stderr\", value: Array.from(arguments).join(\"\") + \"\\n\"});\n}\n"
+               "Module.print = function() { \n   Module.simulatorSteps = Module.simulatorSteps || [];\n   Module.simulatorSteps.push({ action: \"stdout\", value: Array.from(arguments).join(\"\") + \"\\\\n\\n\"});\n}\n"
+               "Module.printErr = function() { \n   Module.simulatorSteps = Module.simulatorSteps || [];\n   Module.simulatorSteps.push({ action: \"stderr\", value: Array.from(arguments).join(\"\") + \"\\\\n\\n\"});\n}\n"
                "Module.preRun = Module.preRun || [];\n"
                f"Module.preRun.push(function() {{ Module.simulatorCode = {c_code_escaped} }})")
     js_file = open(file_path2, "w")
