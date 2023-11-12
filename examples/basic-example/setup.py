74fa3014f4b6f4377c38505be7ab054dd9540621
Ole van Santen <ovs@netcompany.com>
2023-11-12 10:13:01 +0100

    Deleting temporary files

--- examples/basic-example/setup.py
+++
@@ -1,6 +0,0 @@
-import os 
-import shutil
-
-shutil.copyfile('./scripts/index.html', './scripts/example/index.html')
-
-os.system('emcc ./scripts/example/temp.c -s WASM=1 -o ./scripts/example/output.js -s "EXPORTED_FUNCTIONS=[\'_main\']" --js-library ./scripts/library.js')
